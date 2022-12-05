136e389412478170d1cedc764ffaed5c36c7c2fc
jamiehun <jamiesunghun@gmail.com>
2022-12-05 02:35:18 +0000

    fix : supplemental_kill(hash_empty)

--- userprog/process.c
+++ userprog/process.c
@@ -485,7 +485,8 @@ process_cleanup(void)
 	struct thread *curr = thread_current();
 
 #ifdef VM
-	supplemental_page_table_kill(&curr->spt);
+	if(!hash_empty(&curr->spt.table)){
+		supplemental_page_table_kill(&curr->spt);}
 #endif
 
 	uint64_t *pml4;
